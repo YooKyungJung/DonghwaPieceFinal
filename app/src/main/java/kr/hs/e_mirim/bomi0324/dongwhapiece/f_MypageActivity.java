d9f7d95d665b588459e843c7f64d3827a232bfaa
YooKyungJung <yugyungs200@naver.com>
2017-11-23 03:49:42 +0900

    Mypage - tag 5개 firebase 값 read

--- app/src/main/java/kr/hs/e_mirim/bomi0324/dongwhapiece/f_MypageActivity.java
+++ app/src/main/java/kr/hs/e_mirim/bomi0324/dongwhapiece/f_MypageActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,11 +29,14 @@ import org.w3c.dom.Text;
 public class f_MypageActivity extends Fragment implements View.OnClickListener {
     Button back;
     String uid;
+    private String nameStr, tagStrAll;
+    private String tagStr[]=new String[6];
     private DatabaseReference databaseReference;
     private FirebaseDatabase firebaseDatabase;
-    private DatabaseReference fb;
+    private DatabaseReference nameView;
+    private DatabaseReference tagView[]=new DatabaseReference[5];
     private Button wantSee;
-    TextView tname,tag;
+    private TextView name,tag;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView=(ViewGroup)inflater.inflate(R.layout.fragment_mypage, container, false);
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -52,9 +57,17 @@ public class f_MypageActivity extends Fragment implements View.OnClickListener {
             uid = user.getUid();
         }
 
-        fb=databaseReference.child("userData").child(uid).child("nickname");
+        nameView=databaseReference.child("userData").child(uid).child("nickname");
+        tagView[0]=databaseReference.child("userData").child(uid).child("type1");
+        tagView[1]=databaseReference.child("userData").child(uid).child("type2");
+        tagView[2]=databaseReference.child("userData").child(uid).child("type3");
+        tagView[3]=databaseReference.child("userData").child(uid).child("type4");
+        tagView[4]=databaseReference.child("userData").child(uid).child("type5");
+
+
+
         wantSee=(Button)rootView.findViewById(R.id.wantSee);
-        tname=(TextView)rootView.findViewById(R.id.nameText);
+        name=(TextView)rootView.findViewById(R.id.nameText);
         tag=(TextView)rootView.findViewById(R.id.tagText);
 
 
@@ -69,17 +82,67 @@ public class f_MypageActivity extends Fragment implements View.OnClickListener {
     @Override
     public void onStart() {
         super.onStart();
-        fb.addValueEventListener(new ValueEventListener() {
+        nameView.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String name=dataSnapshot.getValue(String.class);
-                tname.setText(name);
+                nameStr=dataSnapshot.getValue(String.class);
+                name.setText(nameStr);
             }
-
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+
+        tagView[0].addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tagStr[0]=dataSnapshot.getValue(String.class);
+                tagStr[5]="#"+tagStr[0];
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+
+        tagView[1].addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tagStr[1]=dataSnapshot.getValue(String.class);
+                tagStr[5]+=" #"+tagStr[1];
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+
+        tagView[2].addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tagStr[2]=dataSnapshot.getValue(String.class);
+                tagStr[5]+=" #"+tagStr[2];
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+
+        tagView[3].addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tagStr[3]=dataSnapshot.getValue(String.class);
+                tagStr[5]+=" #"+tagStr[3];
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+
+        tagView[4].addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tagStr[4]=dataSnapshot.getValue(String.class);
+                tagStr[5]+=" #"+tagStr[4];
+                Toast.makeText(getActivity(), tagStr[5], Toast.LENGTH_SHORT).show();
+                tag.setText(tagStr[5]);
 
             }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
         });
 
     }
