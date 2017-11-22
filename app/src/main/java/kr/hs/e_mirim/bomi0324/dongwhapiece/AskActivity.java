e3817506c983199b70dbd64698a1af84053414db
YooKyungJung <yugyungs200@naver.com>
2017-11-23 03:12:59 +0900

    Mypage 부분 nickname firebase에서 값 가져옴

--- app/src/main/java/kr/hs/e_mirim/bomi0324/dongwhapiece/AskActivity.java
+++ app/src/main/java/kr/hs/e_mirim/bomi0324/dongwhapiece/AskActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -26,6 +27,7 @@ public class AskActivity extends AppCompatActivity {
     private String uid;
     private DatabaseReference databaseReference;
     private FirebaseDatabase firebaseDatabase;
+    private EditText nickname;
     private RadioGroup rg[]=new RadioGroup[5];
     private RadioButton rd[]=new RadioButton[5];
     private String selectedValue[]=new String[5];
@@ -61,6 +63,8 @@ public class AskActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //체크한 값 가져옴
+                nickname=(EditText)findViewById(R.id.nickname);
+                databaseReference.child("userData").child(uid).child("nickname").setValue(nickname.getText().toString());
                 for(int i=0; i<5; i++) {
                     rg[i] = (RadioGroup)findViewById((R.id.radioGroup1)+i);
                     rd[i] = (RadioButton)findViewById(rg[i].getCheckedRadioButtonId());
--- app/src/main/java/kr/hs/e_mirim/bomi0324/dongwhapiece/f_MypageActivity.java
+++ app/src/main/java/kr/hs/e_mirim/bomi0324/dongwhapiece/f_MypageActivity.java
@@ -1,12 +1,24 @@
 package kr.hs.e_mirim.bomi0324.dongwhapiece;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -14,18 +26,37 @@ import android.widget.Button;
  */
 public class f_MypageActivity extends Fragment implements View.OnClickListener {
     Button back;
-    String sum;
+    String uid;
+    private DatabaseReference databaseReference;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference fb;
     private Button wantSee;
+    TextView tname,tag;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView=(ViewGroup)inflater.inflate(R.layout.fragment_mypage, container, false);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            uid = user.getUid();
+        }
+
+        fb=databaseReference.child("userData").child(uid).child("nickname");
         wantSee=(Button)rootView.findViewById(R.id.wantSee);
-        wantSee.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getActivity(), AddbookActivity.class);
-                startActivity(intent);
-            }
-        });
+        tname=(TextView)rootView.findViewById(R.id.nameText);
+        tag=(TextView)rootView.findViewById(R.id.tagText);
+
 
         //밑에는 인기순 추천순처럼 나타나지만 sum을 찾아서 나타낸다.
         return rootView;
@@ -33,6 +64,23 @@ public class f_MypageActivity extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        fb.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name=dataSnapshot.getValue(String.class);
+                tname.setText(name);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
